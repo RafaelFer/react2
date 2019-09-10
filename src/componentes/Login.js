d0a6130c7afb56c161db446e4db6b0b5b2b4b7cd
Rafael Ferreira <rafael22fs@gmail.com>
2019-09-10 17:06:57 -0300

    aula 5 final

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,27 +1,27 @@
-import React, {Component} from 'react';
-import {browserHistory} from 'react-router';
+import React, { Component } from 'react';
+import {browserHistory} from  'react-router';
 
-export default class Login extends Component{
+export default class Login extends Component {
 
-    constructor(){
-        super()
-        this.state = {msg:''}
+    constructor(props){
+        super(props);        
+        this.state = {msg:this.props.location.query.msg};
     }
 
     envia(event){
         event.preventDefault();
+
         const requestInfo = {
             method:'POST',
             body:JSON.stringify({login:this.login.value,senha:this.senha.value}),
-            headers: new Headers({
-                'Content-type':'application/json'
+            headers:new Headers({
+                'Content-type' : 'application/json' 
             })
         };
 
-        event.preventDefault();
         fetch('//instalura-api.herokuapp.com/api/public/login',requestInfo)
             .then(response => {
-                if(response.ok){
+                if(response.ok) {
                     return response.text();
                 } else {
                     throw new Error('não foi possível fazer o login');
@@ -30,23 +30,23 @@ export default class Login extends Component{
             .then(token => {
                 localStorage.setItem('auth-token',token);
                 browserHistory.push('/timeline');
-            }).catch(error => {
-                this.setState({msg:error.message})
             })
+            .catch(error => {
+                this.setState({msg:error.message});
+            });
     }
 
     render(){
         return (
             <div className="login-box">
-            <h1 className="header-logo">Instalura</h1>
-            <span>{this.state.msg}</span>
+                <h1 className="header-logo">Instalura</h1>
+                <span>{this.state.msg}</span>
                 <form onSubmit={this.envia.bind(this)}>
                     <input type="text" ref={(input) => this.login = input}/>
                     <input type="password" ref={(input) => this.senha = input}/>
                     <input type="submit" value="login"/>
                 </form>
-             </div>
+            </div>
         );
     }
-
 }
